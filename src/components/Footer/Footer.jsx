115f76337522091dfdcecfc530161c4908331114
MEGADROID-X23 <omegadroid4@gmail.com>
2025-05-27 10:07:06 -0700

    u3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 import { ImGithub } from "react-icons/im";
 import { SiLinkedin } from "react-icons/si";
-import { BsTwitter } from "react-icons/bs";
+import { BsTwitter, BsWhatsapp } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
@@ -20,10 +20,10 @@ export const Footer = () => {
               LASOP Team
             </a>{" "}
       <div className="social-links">
-        <Link to="https://twitter.com/aniketsaini98" target="_blank">
-          <BsTwitter />
+        <Link to="https://wa.me/+2348052950689" target="_blank">
+          <BsWhatsapp />
         </Link>
-        <Link to="https://github.com/aniketsaini98" target="_blank">
+        <Link to="https://github.com/MEGADROID-X23" target="_blank">
           <ImGithub />
         </Link>
         <Link to="https://www.linkedin.com/in/aniketsaini65" target="_blank">
